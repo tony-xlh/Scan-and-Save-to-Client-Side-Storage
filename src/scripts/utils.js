0c8f345e37615192163e08aae91a889221cb5eef
xulihang <xulihanghai@163.com>
2023-05-25 16:56:23 +0800

    add functions to operate array items

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -18,3 +18,32 @@ export function getUrlParam(name) {
   var r = window.location.search.substr(1).match(reg);
   if (r != null) return unescape(r[2]); return null;
 }
+
+export function moveItemUp(arr, index) {
+  if (index <= 0 || index > arr.length - 1) { //out of bounds
+    return arr;
+  }else{
+    const item = arr.splice(index,1)[0] //delete the item
+    arr.splice(index - 1,0,item) //add the item
+    return arr;
+  }
+}
+
+export function moveItemDown(arr, index) {
+  if (index < 0 || index + 1 > arr.length - 1) { //out of bounds
+    return arr;
+  }else{
+    const item = arr.splice(index,1)[0] //delete the item
+    arr.splice(index + 1,0,item) //add the item
+    return arr;
+  }
+}
+
+export function removeItem(arr, index) {
+  if (index < 0 || index > arr.length - 1) { //out of bounds
+    return arr;
+  }else{
+    arr.splice(index,1) //delete the item
+    return arr;
+  }
+}
