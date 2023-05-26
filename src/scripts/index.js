8310d2f7495173508216f42f86874d9caa3f34f1
xulihang <xulihanghai@163.com>
2023-05-26 10:12:26 +0800

    list scanned documents on index page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,36 @@
+import localforage  from 'localforage';
 import '../styles/index.scss';
 
-console.log('webpack starterkit index');
+let metadataStore = localforage.createInstance({
+  name: "metadata"
+});
+
+window.onload = function(){
+  loadDocumentsList();
+  registerEvents();
+};
+
+async function loadDocumentsList(){
+  let ul = document.getElementById("document-list");
+  let keys = await metadataStore.keys();
+  for (let index = 0; index < keys.length; index++) {
+    const timestamp = keys[index];
+    let listItem = document.createElement("li");
+    let link = document.createElement("a");
+    link.href = "document-scanner.html?ID="+timestamp;
+    link.target = "_blank";
+    link.innerText = new Date(parseInt(timestamp)).toLocaleString();
+    listItem.appendChild(link);
+    ul.appendChild(listItem);
+  }
+}
+
+function registerEvents(){
+  document.getElementById("new-document-button").addEventListener("click",async function(){
+    newDocument();
+  });
+}
+
+function newDocument(){
+  window.open("document-scanner.html?ID="+Date.now(),"_blank");
+}
