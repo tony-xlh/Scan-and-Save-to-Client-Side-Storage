95fc88c56a7547896af1086629197db57cf44173
xulihang <xulihanghai@163.com>
2023-05-26 10:31:11 +0800

    add delete button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,6 +5,10 @@ let metadataStore = localforage.createInstance({
   name: "metadata"
 });
 
+let imagesStore = localforage.createInstance({
+  name: "images"
+});
+
 window.onload = function(){
   loadDocumentsList();
   registerEvents();
@@ -13,16 +17,35 @@ window.onload = function(){
 async function loadDocumentsList(){
   let ul = document.getElementById("document-list");
   let keys = await metadataStore.keys();
-  for (let index = 0; index < keys.length; index++) {
-    const timestamp = keys[index];
-    let listItem = document.createElement("li");
-    let link = document.createElement("a");
-    link.href = "document-scanner.html?ID="+timestamp;
-    link.target = "_blank";
-    link.innerText = new Date(parseInt(timestamp)).toLocaleString();
-    listItem.appendChild(link);
-    ul.appendChild(listItem);
+  if (keys) {
+    for (let index = 0; index < keys.length; index++) {
+      const timestamp = keys[index];
+      let listItem = document.createElement("li");
+      let link = document.createElement("a");
+      link.href = "document-scanner.html?ID="+timestamp;
+      link.target = "_blank";
+      link.innerText = new Date(parseInt(timestamp)).toLocaleString();
+      listItem.appendChild(link);
+      let btn = document.createElement("button");
+      btn.innerText = "Delete";
+      btn.addEventListener("click",async function(){
+        await deleteDocument(timestamp);
+        listItem.remove();
+      });
+      btn.style.marginLeft = "2em";
+      listItem.append(btn);
+      ul.appendChild(listItem);
+    }
   }
+}
+
+async function deleteDocument(ID){
+  const images = await metadataStore.getItem(ID);
+  for (let index = 0; index < images.length; index++) {
+    const imageID = images[index];
+    await imagesStore.removeItem(imageID);
+  }
+  await metadataStore.removeItem(ID);
 }
 
 function registerEvents(){
